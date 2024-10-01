48f78c8e45b091ccce76405c494d84a6f5d6104a
Tejaswi5i5 <147140488+Tejaswi5i5@users.noreply.github.com>
2024-10-01 15:44:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Scroll effect: Change background color of the navbar on scroll
+window.addEventListener('scroll', function() {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
